ba6edc33ad4a21d546bd3fe21262deff032b1c5d
Ishaan Jog <ishaanjog17@gmail.com>
2026-07-23 15:49:19 +0530

    Update code

--- main.cpp
+++ main.cpp
@@ -1,38 +1,37 @@
 #include <iostream>
-#include <map>
 #include <fstream>
+#include <map>
+
 using namespace std;
 
-int main(){
-        fstream file("input.txt");
-        
-        if(!file){
-            cout << "Error opening file." << endl;
-            return 1;
-        }
+int main() {
+    fstream file("input.txt");
 
-        else{
-            map<char,int> frequency;
-            char ch;
-            while(file.get(ch)){
-                frequency[ch]++;
-            }
+    if(!file) {
+        cout << "File doesn't exist." << endl;
+        return 1;
+    }
 
-            file.close();
+    cout << "Contents of file: " << endl;
+    char ch;
+    map<char, int> freq;
+    while(file.get(ch)) {
+        cout << ch;
+        freq[ch]++;
+    }
+    cout << endl << endl;
+    
+    file.close();
 
-            cout<<"Character Frequency in the file:" << endl;
-            for(auto pair:frequency){
-                if(pair.first == '\n'){
-                    cout<<"\\n : "<<pair.second<<endl;
-                }
-                else if(pair.first == ' '){
-                    cout<<"Space : "<<pair.second<<endl;
-                }
-                else
-                cout<<pair.first<<" : "<<pair.second<<endl;
-            }
-            return 0;
-        }
-
-
+    cout << "Frequency of characters: "<< endl;
+    for(auto pair : freq) {
+        if (pair.first == '\n')
+            cout << "newline: " << pair.second << endl;
+        else if (pair.first == ' ')
+            cout << "space: " << pair.second << endl;
+        else
+            cout << pair.first << ": " << pair.second << endl;
+    }
+    
+    return 0;
 }
